faf1b78fe85fba7c61527c4dc0ea5f606d76114a
moonhee0507 <sjmh0507@naver.com>
2023-08-13 21:12:21 +0900

    :bug: 빌드 window is not defined 에러

--- src/app/useResize.ts
+++ src/app/useResize.ts
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 const useResize = () => {
-    const [width, setWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState(0);
 
     const handleResize = () => setWidth(window.innerWidth);
 
